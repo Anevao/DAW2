4d82151ede2865bd0cf880fdc3d9374d97823c65
Anevao <facebusfalso@gmail.com>
2021-10-15 12:35:07 +0200

    adf

--- repositorios-servidor/ut2arrays/Ejercicio6.php
+++ repositorios-servidor/ut2arrays/Ejercicio6.php
@@ -12,8 +12,9 @@ $peliculas=array('Antonio Banderas'=>array('La piel que habito','Dolor y gloria'
 'Brad Pitt'=>'Erase una vez en...Hollywood',
 'Laura Dern'=>'Historia de un matrimonio');
 foreach($peliculas as $actor=>$peli){
-    //mostrar pelis
-    for ($actor as $p){
+    //mostrar pelis no funciona si hay variables k no son array
+    
+    for ($peli as $p){
     echo " $p ";}
 }
 ?>
