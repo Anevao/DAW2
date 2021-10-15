5e91c60119d294bc40884b795c689e6ffc58ccf7
Anevao <facebusfalso@gmail.com>
2021-10-15 12:30:17 +0200

    php

---
+++ repositorios-servidor/ut2arrays/Ejercicio6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Ejercicio 1 </title>
+</head>
+<body>
+<h1><center> Ejercicio 6</center></h1>
+<br>
+<?php
+$peliculas=array('Antonio Banderas'=>array('La piel que habito','Dolor y gloria'),
+'Brad Pitt'=>'Erase una vez en...Hollywood',
+'Laura Dern'=>'Historia de un matrimonio');
+foreach($peliculas as $actor=>$peli){
+    //mostrar pelis
+    for ($actor as $p){
+    echo " $p ";}
+}
+?>
+<br>
+</body>
+</html>
