c8b0668c392388a5b334fe4180368e1fdf4196fb
Anevao <facebusfalso@gmail.com>
2021-10-18 10:39:21 +0200

    clase

--- repositorios-servidor/ut2arrays/Ejercicio6.php
+++ repositorios-servidor/ut2arrays/Ejercicio6.php
@@ -12,10 +12,12 @@ $peliculas=array('Antonio Banderas'=>array('La piel que habito','Dolor y gloria'
 'Brad Pitt'=>'Erase una vez en...Hollywood',
 'Laura Dern'=>'Historia de un matrimonio');
 foreach($peliculas as $actor=>$peli){
-    //mostrar pelis no funciona si hay variables k no son array
-    
-    for ($peli as $p){
-    echo " $p ";}
+    //is_array devuelve un booleano si es array 
+if(is_array($peli)) {
+    foreach ($peli as $p){
+    echo " $p <br>";}
+
+ }else echo " $peli <br>";
 }
 ?>
 <br>
