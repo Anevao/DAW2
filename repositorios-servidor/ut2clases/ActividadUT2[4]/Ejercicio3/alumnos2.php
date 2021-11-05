c6ecbcd10bd62fd38a53c911294821049cdd46d9
Anevao <facebusfalso@gmail.com>
2021-11-05 12:56:10 +0100

    .

--- repositorios-servidor/ut2clases/ActividadUT2[4]/Ejercicio3/alumnos2.php
+++ repositorios-servidor/ut2clases/ActividadUT2[4]/Ejercicio3/alumnos2.php
@@ -38,38 +38,3 @@ class Segundo extends Alumno2{
     }
 }
 ?>
-/*class Alumno2{
-    private $nom;
-    private $edad;
-    private $nota;
-    const $ciclo ="DAW";
-
-    function __construct($nom, $edad, $nota){
-        this->nom=$nom;
-        this->edad=$edad;
-        this->nota=$nota;
-
-    }/*
-    static function superaCurso($nota,$nom){
-        if($nota>5)
-        echo "el alumno ".$nom."esta aprobado";
-        else 
-        echo "el alumno ".$nom."esta suspenso";
-    }
-
-}
-class Primero extends Alumno2{
-    static function superaCurso($nota,$nom){
-        if($nota>5)
-        echo "el alumno ".$nom."esta aprobado";
-        else 
-        echo "el alumno ".$nom."esta suspenso";
-    }
-}
-class Segundo extends Alumno2{
-    private $fct=false;
-    private $protecto:
-    
-    
-}*/
-?>
--- repositorios-servidor/ut2clases/ActividadUT2[4]/Ejercicio3/indexejempl.php
+++ repositorios-servidor/ut2clases/ActividadUT2[4]/Ejercicio3/indexejempl.php
@@ -9,8 +9,7 @@
 <br>
 <?php
     require_once("./alumnos2.php")
-
-
+    
     $primero1 = new Primero("Luis",20,5);
     echo $primero1->superaCurso();
     $segundo1 = new Segundo("Marta", 22,7,8,"no apto");
