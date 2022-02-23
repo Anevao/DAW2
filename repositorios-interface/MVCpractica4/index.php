61d471dac7ca3858887ad9e1bba7a8d74e1b61ed
Anevao <facebusfalso@gmail.com>
2022-02-23 12:12:37 +0100

    finalmente

--- repositorios-interface/MVCpractica4/index.php
+++ repositorios-interface/MVCpractica4/index.php
@@ -5,7 +5,9 @@
 			{
         header ("location: ./vista/admin.html");
 			}
-		else
+		else if ($_POST['usuario']=='profesor') {
+      header ("location: ./vista/profesor.html");
+    }else
       header ("location: ./vista/alumno.html");
 		exit;
 		}
@@ -18,20 +20,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PRACTICA4</title>
     <style>
+
       @font-face {
         font-family: "news";
         font-style: normal;
         font-weight: 1200;
-        src: url("../source/News.otf"); 
+        src: url("./source/News.otf"); 
       }
-      :hover, :focus {
-        border: 2px solid blue;
+      /*html{
+        height: 100%;
+      }*/
+       :focus {
+        outline: 2px solid blue;
       }
       body {
         display: grid;
         grid-template:
-          "header" 20%
+          "header" 15%
           "main" auto
+          "nav"   10%
           "footer" 10%;
         align-items: center;
       }
@@ -50,28 +57,52 @@
       footer {
         grid-area: footer;
       }
-      @media screen {
+      @media (min-width: 600px) {
         body {
           /*height: 100%;*/
           grid-template:
-            "header " 300px
-            " main  " 100%
-            " footer " 150px /
-            100%;
+            "header header " 150px
+            "main main  " 1fr
+            " footer nav " 100px /
+            50% 50%;
         }
-      }
-      * {
-        margin: 0;
-        padding: 0;
-      }
-      img {
+        img {
         height: 80px;
-        object-fit: contain;
       }
       
-      /* Header y footer */
-      header,
-      footer {
+      }
+      @media (min-width: 1000px) {
+        body {
+         /* background-image: url("./source/centro2.jpg") no-repeat center fixed;
+        object-fit: cover;
+        opacity: 0,5;*/
+          /*height: 100%;*/
+          grid-template:
+            "header header " 150px
+            "main main  " 1fr
+            " footer nav " 100px /
+            80% 20%;
+        }
+        img {
+        height: 80px;
+      }
+      
+      }
+      img {
+        height: 40px;
+        object-fit: contain;
+      }
+      /* Header nav y footer */
+      header {
+        height: 150px;
+        font-family: "news",sans-serif, fantasy;
+        background-color: #80cfff;
+      }
+      h1{
+        font-size: larger;
+      }
+      nav,footer {
+        height: 100px;
         font-family: "news",sans-serif, fantasy;
         background-color: #80cfff;
       }
@@ -79,9 +110,7 @@
       main {
         font-family: "news", sans-serif, fantasy;
         border: 5px ridge lightgray;
-        /*background-image: url("./source/centro2.jpg");
-        object-fit: cover;
-        opacity: 0,5;*/
+        
       }
       /* Footer */
       footer p {
@@ -89,6 +118,7 @@
         margin-top: 50px;
       }
       form{
+        align-items: center;
         border: 3px solid grey;
         display: flex;
       flex-direction: column;
@@ -107,10 +137,8 @@
     </header>
     <nav>
       <ul>
-        <li><a href="#">La academia</a></li>
-        <li><a href="#">Bailes</a></li>
-        <li><a href="#">Horarios</a></li>
-        <li><a href="#">Nosotros</a></li>
+        <li><a href="http://cifpjuandecolonia.centros.educa.jcyl.es/sitio/index.cgi?wid_form=1">Contacto</a></li>
+        <li><a href="#">RRSS</a></li>
       </ul>
     </nav>
     <main>
@@ -131,11 +159,11 @@
       </ul>
       <h2>Acceso al aula virtual.</h2>
       <form action="" method="post" border="2">
-        <legend>formulario de conexion con la plataforma</legend>
+        <legend>Formulario de conexion con la plataforma</legend>
         <!--tenemos un formulario con el action hacia "" osease se queda aqui -->
-        Usuario: <input type="text" name="usuario" placeholder="introduzca usuario(user o admin)" required/> Contraseña:
+        Usuario: <input type="text" name="usuario" placeholder="profesor" required/> Contraseña:
         <input type="password" name="clave" required />
-        <!--el submit coge los input y los envia coge el name como valor de refe y el value como su valor -->
+        <!--el submit coge los input y los envia por ahora el valor name solo acepta admin profesor y el resto los considera alumnos -->
         <input type="submit" name="enviar" value="LOGIN" />
       </form>
     </main>
